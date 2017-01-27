c2f12ce0b1dd8d9d02635983ecea49b95d23a498
Isabelle <icrawfo89@gmail.com>
2017-01-26 19:46:44 -0500

    added details to model

--- app/models/teenager.rb
+++ app/models/teenager.rb
@@ -2,4 +2,17 @@ class Teenager
   include Mongoid::Document
   field :name, type: String
   field :type, type: String
+  field :stats, type: Hash
+  field :powers, type: Array
+  embeds_many :knacks
+  field :traits, type: Array
+  accepts_nested_attributes_for :knacks
+end
+
+class Knack
+  include Mongoid::Document
+  belongs_to :teenager
+  field :name, type: String
+  field :bonus, type: Integer
+  field :stat, type: String
 end
